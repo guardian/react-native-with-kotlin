959c45fd3a084e87017773dd92dbb98866741bcd
Max Spencer <mrmaxspencer@googlemail.com>
2019-06-25 14:25:00 +0100

    Mission statement added

--- App.js
+++ App.js
@@ -21,8 +21,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.welcome}>TODO!</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
